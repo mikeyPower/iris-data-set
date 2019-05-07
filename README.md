020e78d977c39d7775b612e9b0edab4dbade53a5
mikeyPower <powerm3@tcd.ie>
2019-05-07 15:58:42 +0000

    adding comments

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ https://archive.ics.uci.edu/ml/datasets/Iris
 
 https://en.wikipedia.org/wiki/Iris_flower_data_set
 
+https://www.statisticshowto.datasciencecentral.com/probability-and-statistics/correlation-coefficient-formula/
+
+https://stackoverflow.com/questions/49179155/python-class-correlation/49183888
+
+https://www.datascience.com/blog/introduction-to-correlation-learn-data-science-tutorials
+
+https://www.khanacademy.org/math/probability/data-distributions-a1/summarizing-spread-distributions/a/calculating-standard-deviation-step-by-step
 
 never assign max and min to 0 as 0 might not exist in the data
--- iris_analysis.py
+++ iris_analysis.py
@@ -2,60 +2,117 @@
 
 import math
 
+
+# Function to calculate the correlation between to sets of data
+def correlation(data, column_a, column_b):
+
+    # Find the mean of the two columns we are going to get the correlation for
+    sum_x=0.0
+    sum_y=0.0
+    for i in data:
+        sum_x+=i[column_a]
+        sum_y+=i[column_b]
+
+    avg_x = sum_x/len(data)
+    avg_y = sum_y/len(data)
+
+    # Calculate the a - mean & b - mean for all the values in the list
+    a=[]
+    b=[]
+    for i in data:
+        a.append(i[column_a]-avg_x)
+        b.append(i[column_b]-avg_y)
+
+    # Calculate a*b, a^2 & b^2 for all values in both columns
+    a_b=0.0
+    a_squared=0.0
+    b_squared=0.0
+    for i in range(len(data)):
+        a_b+=a[i]*b[i]
+        a_squared+=a[i]*a[i]
+        b_squared+=b[i]*b[i]
+
+    # Work out the square root of a^2 x b^2
+    a_b_sqrt = math.sqrt(a_squared*b_squared)
+
+    # Divide a*b to the answer above
+    return(a_b/a_b_sqrt)
+
+
+
+
 def standard_dev(flower,mean,column):
 
+    # Calculate the standard deviation by summing the each value from subtracted by the mean and squaring the result
     total = 0.0
     for i in flower:
+
         sum=i[column]-mean
+
         square=sum*sum
         total+=square
 
+    # divide the toal by the population
     s_dev = total/len(flower)
+
+    # return the square root to get the standard deviation
     return math.sqrt(s_dev)
 
 
 
 def main():
 
+    # Open the file
     file = open("iris.data", "r")
+
+    # read each line of the file and put it into a list
     lines = file.readlines()
-    data =[]
+
+    # Initalize some variables
+    flower_data =[]
     iris_setosa=[]
     iris_versicolour=[]
     iris_virginica=[]
 
+    # Remove known errors from data
     errors = []
     errors.append(lines[34])
     errors.append(lines[37])
     lines.pop(34)
     lines.pop(37)
 
-
+    # Loop through all lines except the last line as it's empty
     for line in lines[:-1]:
+
+        # For each line remove any trailing whitespace e.g. newline characters \n and split it by , creating a list
         row = line.rstrip().split(',')
-        data.append(row)
+
+        # Seperate each list based on the species
         if(row[4]=="Iris-setosa"):
-            iris_setosa.append([float(x) for x in row[:-1]])
+            iris_setosa.append([float(x) for x in row[:-1] + [1.0]])
+
+            # Add some sort of indicator for each species e.g. 1.0 for iris setosa etc..
+            flower_data.append([float(x) for x in row[:-1] + [1.0]])
         elif(row[4]=="Iris-versicolor"):
 
-            iris_versicolour.append([float(x) for x in row[:-1]])
+            iris_versicolour.append([float(x) for x in row[:-1] + [2.0]])
+            flower_data.append([float(x) for x in row[:-1] + [2.0]])
         else:
-            iris_virginica.append([float(x) for x in row[:-1]])
+            iris_virginica.append([float(x) for x in row[:-1] + [3.0]])
+            flower_data.append([float(x) for x in row[:-1] + [3.0]])
 
+    # calculate size of population for each species
     setosa_len = len(iris_setosa)
     versicolour_len = len(iris_versicolour)
     virginica_len = len(iris_virginica)
 
-   # print(len(data))
-    #print(len(iris_setosa))
-    #print(len(iris_versicolour))
-    #print(len(iris_virginica))
-
+    # Initalize some varialbles
     setosa_sepal_length_avg=0
     setosa_sepal_width_avg=0
     setosa_petal_length_avg=0
     setosa_petal_width_avg=0
 
+    # Shouldn't initalize the below variables to 0 as 0 may not be part of the population giving us incorrect results so initalize to first relevant data
     setosa_sepal_length_max=iris_setosa[0][0]
     setosa_sepal_width_max=iris_setosa[0][1]
     setosa_petal_length_max=iris_setosa[0][2]
@@ -66,11 +123,7 @@ def main():
     setosa_petal_length_min=iris_setosa[0][2]
     setosa_petal_width_min=iris_setosa[0][3]
 
-    #for i in iris_setosa:
-        #print(i)
-
-
-
+    # Calculate the average, min and max of iris_setosa and other species
     for i in iris_setosa:
         setosa_sepal_length_avg+=i[0]
         setosa_sepal_width_avg+=i[1]
@@ -157,11 +210,6 @@ def main():
 
 
 
-
-
-
-
-
     virginica_sepal_length_avg=0
     virginica_sepal_width_avg=0
     virginica_petal_length_avg=0
@@ -224,19 +272,18 @@ def main():
     print("Minimum petal legth :",setosa_petal_length_min)
     print("Minimum petal width :",setosa_petal_width_min)
 
-    print("Standard Deviation sepal length :",standard_dev(iris_setosa,setosa_sepal_length_avg,0))
-    print("Standard Deviation sepal width :",standard_dev(iris_setosa,setosa_sepal_width_avg,1))
-    print("Standard Deviation petal legth :",standard_dev(iris_setosa,setosa_petal_length_avg,2))
-    print("Standard Deviation petal width :",standard_dev(iris_setosa,setosa_petal_width_avg,3))
+    print("Standard Deviation sepal length :",standard_dev(iris_setosa,setosa_sepal_length_avg/setosa_len,0))
+    print("Standard Deviation sepal width :",standard_dev(iris_setosa,setosa_sepal_width_avg/setosa_len,1))
+    print("Standard Deviation petal legth :",standard_dev(iris_setosa,setosa_petal_length_avg/setosa_len,2))
+    print("Standard Deviation petal width :",standard_dev(iris_setosa,setosa_petal_width_avg/setosa_len,3))
 
     print()
 
-
     print("Versicolour Data :")
-    print("Average sepal length :", versicolour_sepal_length_avg/setosa_len)
-    print("Average sepal width :",versicolour_sepal_width_avg/setosa_len)
-    print("Average petal length :",versicolour_petal_length_avg/setosa_len)
-    print("Average petal width :",versicolour_petal_width_avg/setosa_len)
+    print("Average sepal length :", versicolour_sepal_length_avg/versicolour_len)
+    print("Average sepal width :",versicolour_sepal_width_avg/versicolour_len)
+    print("Average petal length :",versicolour_petal_length_avg/versicolour_len)
+    print("Average petal width :",versicolour_petal_width_avg/versicolour_len)
 
     print("Max sepal length :",versicolour_sepal_length_max)
     print("Max sepal width :",versicolour_sepal_width_max)
@@ -248,18 +295,18 @@ def main():
     print("Minimum petal legth :",versicolour_petal_length_min)
     print("Minimum petal width :",versicolour_petal_width_min)
 
-    print("Standard Deviation sepal length :",standard_dev(iris_versicolour,setosa_sepal_length_avg,0))
-    print("Standard Deviation sepal width :",standard_dev(iris_versicolour,setosa_sepal_width_avg,1))
-    print("Standard Deviation petal legth :",standard_dev(iris_versicolour,setosa_petal_length_avg,2))
-    print("Standard Deviation petal width :",standard_dev(iris_versicolour,setosa_petal_width_avg,3))
+    print("Standard Deviation sepal length :",standard_dev(iris_versicolour, versicolour_sepal_length_avg/versicolour_len,0))
+    print("Standard Deviation sepal width :",standard_dev(iris_versicolour, versicolour_sepal_width_avg/versicolour_len,1))
+    print("Standard Deviation petal legth :",standard_dev(iris_versicolour, versicolour_petal_length_avg/versicolour_len,2))
+    print("Standard Deviation petal width :",standard_dev(iris_versicolour, versicolour_petal_width_avg/versicolour_len,3))
 
     print()
 
     print("Virginica Data :")
-    print("Average sepal length :", virginica_sepal_length_avg/setosa_len)
-    print("Average sepal width :",virginica_sepal_width_avg/setosa_len)
-    print("Average petal length :",virginica_petal_length_avg/setosa_len)
-    print("Average petal width :",virginica_petal_width_avg/setosa_len)
+    print("Average sepal length :", virginica_sepal_length_avg/virginica_len)
+    print("Average sepal width :",virginica_sepal_width_avg/virginica_len)
+    print("Average petal length :",virginica_petal_length_avg/virginica_len)
+    print("Average petal width :",virginica_petal_width_avg/virginica_len)
 
     print("Max sepal length :",virginica_sepal_length_max)
     print("Max sepal width :",virginica_sepal_width_max)
@@ -271,11 +318,17 @@ def main():
     print("Minimum petal legth :",virginica_petal_length_min)
     print("Minimum petal width :",virginica_petal_width_min)
 
-    print("Standard Deviation sepal length :",standard_dev(iris_virginica,setosa_sepal_length_avg,0))
-    print("Standard Deviation sepal width :",standard_dev(iris_virginica,setosa_sepal_width_avg,1))
-    print("Standard Deviation petal legth :",standard_dev(iris_virginica,setosa_petal_length_avg,2))
-    print("Standard Deviation petal width :",standard_dev(iris_virginica,setosa_petal_width_avg,3))
+    print("Standard Deviation sepal length :",standard_dev(iris_virginica,virginica_sepal_length_avg/virginica_len,0))
+    print("Standard Deviation sepal width :",standard_dev(iris_virginica,virginica_sepal_width_avg/virginica_len,1))
+    print("Standard Deviation petal legth :",standard_dev(iris_virginica,virginica_petal_length_avg/virginica_len,2))
+    print("Standard Deviation petal width :",standard_dev(iris_virginica,virginica_petal_width_avg/virginica_len,3))
+
+    print()
 
 
+    print("Correlation between Species and Sepal Length :",correlation(flower_data,0,4))
+    print("Correlation between Species and Sepal Width :",correlation(flower_data,1,4))
+    print("Correlation between Species and Petal Length :",correlation(flower_data,2,4))
+    print("Correlation between Species and Petal Width :",correlation(flower_data,3,4))
 if __name__ == '__main__':
     main()
